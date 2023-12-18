acc76e97ad16bb1514ca7d5769d77b27e0ba27cc
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2023-12-17 23:06:51 -0300

    Create Battles.cs

---
+++ Battles.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Battles{
+
+    class Battle_F1{
+        public static void Battle1(Player jogador){
+            Console.WriteLine("Hello World! \nWellcome to de battles namespace");
+            Console.WriteLine($"{jogador.getName()}, abaixo seguem seus atributos atualizados:");
+        
+        }
+    }
+}
