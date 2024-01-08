e0372d7aa0c6649a562750c080f62194f14bca80
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2024-01-08 09:17:13 -0300

    Update Battles.cs

--- Battles.cs
+++ Battles.cs
@@ -6,7 +6,16 @@ namespace Battles{
         public static void Battle1(Player jogador){
             Console.WriteLine("Hello World! \nWellcome to de battles namespace");
             Console.WriteLine($"{jogador.getName()}, abaixo seguem seus atributos atualizados:");
-        
+            Console.WriteLine($"Vida: {jogador.getHeath()}");
+            Console.WriteLine($"Defesa: {jogador.getDefesa()}");
+            Console.WriteLine($"Ataque: {jogador.getAtack()}");
+            Console.WriteLine($"Doblons: {jogador.getDoblons()}");
+            Principal.Separador();
+
+            Console.WriteLine("Precione qualquer tecla para continuar.");
+            Console.ReadKey();
+            Principal.Separador();
+
         }
     }
 }
